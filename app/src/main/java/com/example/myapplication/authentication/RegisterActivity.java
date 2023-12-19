d0091ae8d690b3e6101e9d91d7896d77f7c12574
Phucccc ne <phuclalami@gmail.com>
2023-12-19 18:21:25 +0700

    update register

--- app/src/main/java/com/example/myapplication/authentication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/authentication/RegisterActivity.java
@@ -1,14 +1,22 @@
 package com.example.myapplication.authentication;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
@@ -18,16 +26,25 @@ import com.example.myapplication.models.UserModel;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class RegisterActivity extends AppCompatActivity {
     private static final String TAG = "RegisterActivity";
     private EditText fullname;
     private final UserModel userModel = new UserModel();
     private EditText phone;
     private EditText email;
+    private Context context;
+
+
     private EditText password;
     private EditText khuvuc;
-
+    private EditText birthday;
     private Button registerUser;
+    private RadioButton fmale;
+    private RadioButton male;
+    private RadioGroup gender;
 
     public RegisterActivity() {
     }
@@ -37,6 +54,15 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         init();
+        ImageButton bd = findViewById(R.id.ig_bd);
+        context = this;
+        bd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePickerDialog();
+            }
+        });
+
         registerUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,6 +71,19 @@ public class RegisterActivity extends AppCompatActivity {
                 String txt_email = email.getText().toString();
                 String txt_password = password.getText().toString();
                 String txt_khuvuc = khuvuc.getText().toString();
+                String txt_bd = birthday.getText().toString();
+                final String[] txt_gender = new String[2];
+                gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+                {
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        if (checkedId == R.id.radioF) {
+                            txt_gender[0] = "Female";
+                        } else if (checkedId == R.id.radioM) {
+                            txt_gender[0] = "Female";
+                        }
+                    }
+                });
                 Log.d("Register User",txt_phone);
                 if(TextUtils.isEmpty(txt_fullname) || TextUtils.isEmpty(txt_phone)|| TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password) || TextUtils.isEmpty(txt_khuvuc)){
                     Toast.makeText(RegisterActivity.this,"Empty credentials",Toast.LENGTH_LONG).show();
@@ -52,20 +91,21 @@ public class RegisterActivity extends AppCompatActivity {
                     Toast.makeText(RegisterActivity.this, "Password to short", Toast.LENGTH_LONG).show();
                 } else
                 {
-//                    userModel.register(txt_email, txt_phone, txt_password, txt_khuvuc, new UserModel.RegisterCallbacks() {
-//                        @Override
-//                        public void onSuccess(User user) {
-//                            Toast.makeText(RegisterActivity.this,"Successful account registration",Toast.LENGTH_LONG).show();
-//                            getSupportFragmentManager().beginTransaction()
-//                                    .replace(R.id.fragment_container, new HomeFragment(user.getUuid()))
-//                                    .commit();
-//                        }
-//                        @Override
-//                        public void onFailed(Exception e) {
-//                            Snackbar.make(view, "Register fail", Snackbar.LENGTH_LONG).show();
-//
-//                        }
-//                    });
+
+                    userModel.register(txt_email, txt_phone, txt_password,txt_fullname ,txt_khuvuc,txt_bd,txt_gender[0], new UserModel.RegisterCallbacks() {
+                        @Override
+                        public void onSuccess(User user) {
+                            Toast.makeText(RegisterActivity.this,"Successful account registration",Toast.LENGTH_LONG).show();
+                            getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.fragment_container, new HomeFragment(user.getUuid()))
+                                    .commit();
+                        }
+                        @Override
+                        public void onFailed(Exception e) {
+                            Snackbar.make(view, "Register fail", Snackbar.LENGTH_LONG).show();
+
+                        }
+                    });
 //                    userModel.checkUserIsExists(txt_phone, new UserModel.CheckExistsCallbacks() {
 //                        @Override
 //                        public void onExists() {
@@ -74,10 +114,13 @@ public class RegisterActivity extends AppCompatActivity {
 //
 //                        @Override
 //                        public void onNotFound() {
-//                            userModel.register(txt_email, txt_phone, txt_password, txt_khuvuc, new UserModel.RegisterCallbacks() {
+//                            userModel.register(txt_email, txt_phone, txt_password,txt_fullname ,txt_khuvuc,txt_bd,txt_gender[0], new UserModel.RegisterCallbacks() {
 //                                @Override
 //                                public void onSuccess(User user) {
-//                                    Log.d("user", user.toString());
+//                                    Toast.makeText(RegisterActivity.this,"Successful account registration",Toast.LENGTH_LONG).show();
+//                                    getSupportFragmentManager().beginTransaction()
+//                                            .replace(R.id.fragment_container, new HomeFragment(user.getUuid()))
+//                                            .commit();
 //                                }
 //                                @Override
 //                                public void onFailed(Exception e) {
@@ -97,11 +140,38 @@ public class RegisterActivity extends AppCompatActivity {
         email = findViewById(R.id.et_email);
         password = findViewById(R.id.et_password);
         khuvuc = findViewById(R.id.et_khuvuc);
+        birthday = findViewById(R.id.et_bd);
+        gender = findViewById(R.id.radioGrp);
         registerUser = findViewById(R.id.btn_to_register);
     }
     private void swapLogin(){
         Intent sw = new Intent(this, LoginActivity.class);
         startActivity(sw);
+    }
+
+    private void showDatePickerDialog() {
+        // Lấy ngày và thời gian hiện tại
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        // Tạo đối tượng DatePickerDialog
+        DatePickerDialog datePickerDialog = new DatePickerDialog(context,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        calendar.set(Calendar.DAY_OF_MONTH,day);
+                        calendar.set(Calendar.MONTH,month);
+                        calendar.set(Calendar.YEAR, year);
+                        birthday.setText(String.valueOf(day)+"/"+String.valueOf(month+1)+"/"+String.valueOf(year));
+
+                    }
+
+                },
+                dayOfMonth, month, year);
+        // Hiển thị DatePickerDialog
+        datePickerDialog.show();
+
     }
 
 }
